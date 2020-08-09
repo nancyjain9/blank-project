0374278d90e4839b2e4f1bd4cb5020264672043a
nancy jain <nancy.jain@avizva.com>
2020-08-09 13:01:56 +0530

    added project boilerpate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <section >
+        <main>
+            I am content
+        </main>
+        <footer></footer>
+    </section>
+  );
+}
+
+export default App;
